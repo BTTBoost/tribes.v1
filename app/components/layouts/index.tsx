69f0fab67990597b60161d71f7fe9fae82954d5b
Aditya Veer <nakulparmar15@gmail.com>
2022-03-14 16:35:04 +0530

    experiments

--- app/components/layouts/index.tsx
+++ app/components/layouts/index.tsx
@@ -14,15 +14,12 @@ interface Props {
 
 const OuterDiv = styled.div`
   position: relative;
-  min-height: 100vh;
   background-color: #000f29;
   width: 100%;
   letter-spacing: 0.025em;
   line-height: 1.5;
   color: #eaeaea;
   font-size: 1.5rem;
-  display: flex;
-  flex-direction: row;
   overflow-x: hidden;
 `;
 
@@ -46,12 +43,8 @@ const Layout = ({ children }: Props) => {
   }, []);
   return (
     <OuterDiv>
-      <Sidebar />
-      <InnerDiv>
-        <Navbar />
-        <Main>{children}</Main>
-        {/* <Footer /> */}
-      </InnerDiv>
+      <Main>{children}</Main>
+      {/* <Footer /> */}
     </OuterDiv>
   );
 };
